cc5ea1a3024ec1715d20eb283f608d04fc55fe45
legendslayer01 <ajay.satishpriya@gmail.com>
2022-04-09 00:21:58 +0530

    ✨ add Auth,Student

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const mysql = require('mysql');
+//const mysql = require('mysql');
 const TransactionRoutes = require('./routes/TransactionRoutes');
+const AuthRoutes = require('./routes/Auth');
 const dotenv = require('dotenv');
+const StudentRoutes = require('./routes/Student');
 const cors = require('cors');
 dotenv.config({ path: 'config.env' });
 const app = express();
@@ -11,7 +13,9 @@ app.get('/', (req, res) => {
   res.send('Home');
 });
 
-app.use('/transac', TransactionRoutes);
+//app.use('/transac', TransactionRoutes);
+app.use('/', AuthRoutes);
+app.use('/dashboard', StudentRoutes);
 
 app.listen(3001, () => {
   console.log('✅ Server is started at port: 3001');
