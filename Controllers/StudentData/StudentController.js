ddf3842ce803742e349d97dfdd1cc867e12a2e9f
Shorya <rawatshorya@gmail.com>
2022-04-16 20:18:10 +0530

    ✨  student data retrieval

--- Controllers/StudentData/StudentController.js
+++ Controllers/StudentData/StudentController.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const res = require('express/lib/response');
-const StudentDB = require('../../Model/StudentData');
-//const Reqemail=req.body.email;
+const express = require("express");
+const StudentDB = require("../../Model/StudentData");
 //save in cookies or body then fetch
-const Fetch = async (email) => {
-  const data = await StudentDB.findOne({ email });
-  console.log(data);
-  res.send({ data });
+const Fetch = async (req, res) => {
+  // const Reqemail = req.body.email;
+  const data = await StudentDB.findOne({ email: req.body.email });
+  if (data != null) {
+    console.log(data);
+    res.send({ status: true, message: "Retrieve Successful", data });
+  } else {
+    res.status(404).send({ status: false, message: "Retrieval failed" });
+  }
 };
 module.exports = Fetch;
